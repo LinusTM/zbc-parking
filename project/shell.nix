0e7deb5c5a35a2e45c85841a7a2e7a5d8d2a15e3
Emil <emil@Emils-MacBook-Pro.local>
2022-06-13 09:44:34 +0200

    Fixed small mistake.

--- project/shell.nix
+++ project/shell.nix
@@ -2,7 +2,7 @@
 
 with pkgs;
 
-let myWerkzeug = python39Packages.werkzeug.overridePythonAttrs (oldAttrs: rec {
+let myWerkzeug = python310Packages.werkzeug.overridePythonAttrs (oldAttrs: rec {
 			postPatch = ''
 		substituteInPlace src/werkzeug/_reloader.py \
 			--replace "rv = [sys.executable]" "return sys.argv"
