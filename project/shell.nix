df29fdc163ca9e851ca8fc4254a245626330c4ba
Emil <emil@Emils-MacBook-Pro.local>
2022-06-13 12:23:28 +0200

    Unfucked shell.nix.

--- project/shell.nix
+++ project/shell.nix
@@ -2,7 +2,7 @@
 
 with pkgs;
 
-let myWerkzeug = python310Packages.werkzeug.overridePythonAttrs (oldAttrs: rec {
+let myWerkzeug = python39Packages.werkzeug.overridePythonAttrs (oldAttrs: rec {
 			postPatch = ''
 		substituteInPlace src/werkzeug/_reloader.py \
 			--replace "rv = [sys.executable]" "return sys.argv"
@@ -11,16 +11,10 @@ let myWerkzeug = python310Packages.werkzeug.overridePythonAttrs (oldAttrs: rec {
 in mkShell {
 	buildInputs = [
 		nodePackages.pyright
-		python310
+		python39
 		python39Packages.psycopg2
-		python310Packages.flask
-<<<<<<< HEAD
+		python39Packages.flask
 		python39Packages.wtforms
-		python39Packages.requests
-		python39Packages.flask_wtf
-=======
-		python310Packages.psycopg2
->>>>>>> 0e7deb5c5a35a2e45c85841a7a2e7a5d8d2a15e3
 		myWerkzeug
 	];
 
