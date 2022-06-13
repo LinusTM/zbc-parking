7b6fb58bf4596e2d17715ad12d5fa4498872c407
Emil <emil@Emils-MacBook-Pro.local>
2022-06-13 04:46:05 +0200

    Improved nix-shell, and added .direnv to gitignore.

--- project/shell.nix
+++ project/shell.nix
@@ -2,14 +2,22 @@
 
 with pkgs;
 
-mkShell {
+let myWerkzeug = python39Packages.werkzeug.overridePythonAttrs (oldAttrs: rec {
+			postPatch = ''
+		substituteInPlace src/werkzeug/_reloader.py \
+			--replace "rv = [sys.executable]" "return sys.argv"
+	''; });
+
+in mkShell {
 	buildInputs = [
 		nodePackages.pyright
 		python310
 		python310Packages.flask
+		myWerkzeug
 	];
 
 	shellHook = ''
 	export FLASK_APP=main
+	export FLASK_ENV=development
 	'';
 }
