81ffe4c2c62ba29e98820cca7771a15201594b51
LinusTD <9331149-LinusTD@users.noreply.gitlab.com>
2022-06-13 12:06:59 +0200

    added logic for admin page + packages

--- project/registration.py
+++ project/registration.py
@@ -1,7 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from wtforms import Form, SelectField, StringField, validators
 
 registration = Blueprint('registration', __name__)
 
-@registration.route("/registration")
+class RegistrationForm(Form):
+    fname   = StringField('fname', [validators.Length(min=4, max=25)])
+    lname   = StringField('lname', [validators.Length(min=4, max=25)])
+    email   = StringField('Email', [validators.Length(min=8, max=35)])
+    role    = SelectField('role', choices=[(1, "Gæst"), (2, "Medarbejder"), (3, "Elev")])
+
+@registration.route("/registration", methods=['GET', 'POST'])
 def registration_page():
-	return render_template("registration.html", test_var="test")
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate():
+        person.fname = form.fname.data 
+        person.lname = form.lname.data 
+        person.email = form.email.data
+        person.role = form.role.data
+
+        # Send values to database
+        cur.execute("""
+            INSERT INTO people(fname, lname, email, role_id)
+            VALUES (%S, %S, %S, %S)
+            """,
+            (person.fname, person.lname, person.email, person.role))
+
+    return render_template("registration.html", form=form)
+
