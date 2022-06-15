022f7c40aa28b456863466c965125238bcd2490a
Emil <emil@Emils-MacBook-Pro.local>
2022-06-15 13:28:37 +0200

    Added back and improved shell.nix.

---
+++ project/shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let myWerkzeug = python39Packages.werkzeug.overridePythonAttrs (oldAttrs: rec {
+			postPatch = ''
+			substituteInPlace src/werkzeug/_reloader.py \
+			--replace "rv = [sys.executable]" "return sys.argv"
+	''; });
+
+in mkShell {
+	buildInputs = [
+		nodePackages.pyright
+		myWerkzeug
+		python39Packages.psycopg2
+		python39Packages.flask
+		python39Packages.wtforms
+	];
+
+	shellHook = ''
+	export FLASK_APP=main
+	export FLASK_ENV=development
+	'';
+}
