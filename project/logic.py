dbaf0c66234cb497bbfa0a993e184b2d7a54f1d3
CarloMercuri <cmercuri85@gmail.com>
2022-06-16 13:37:23 +0200

    Update logic.py

--- project/logic.py
+++ project/logic.py
@@ -4,6 +4,9 @@ from dbaccess import *
 import re
 from datetime import *
 # import pandas as pd
+import math
+import dataclasses
+import json
 
 
 
@@ -67,10 +70,29 @@ def RegisterEntrance(parkbizz_serial, timestamp):
     return
 
 def RegisterExit(parkbizz_serial, timestamp):
+    exit = ParkingActivity(parkbizz_serial, timestamp, 'EXIT')
+    CalculatePayment(parkbizz_serial, exit)
     # receipt
     InsertExit(parkbizz_serial, timestamp)
     return
 
+
+def CalculatePayment(parkbizz_serial, exit):
+    entrance = GetLatestEntrance(parkbizz_serial)
+    print(f'Entrance: {entrance}')
+    print(f'Exit: {exit}')
+    diff = exit.time - entrance.time
+    hours = math.floor(diff.total_seconds() / 60 / 60)
+
+    # cap at 8
+    if hours > 8:
+        hours = 8
+    
+    if hours < 0:
+        hours = 0
+
+    payment = 4 + (4 * hours)
+    InsertReceipt(GetAccountNumber(parkbizz_serial), 30, entrance.time, exit.time, exit.time, payment)
 
 def GenerateBizzActivity(serial):
     start_date = datetime(2022, 3, 12, 8, 0, 0)
@@ -83,6 +105,17 @@ def GenerateBizzActivity(serial):
             RegisterExit(serial, time_now)
             time_now = time_now + timedelta(minutes=random.randint(30, 180))
         start_date = start_date + timedelta(days=1)
+        
+def GetAllParkingBizzes():
+    return GetParkingBizzes()
+    
+def GenerateParkbizzes():
+    accounts = GetAccounts()
+    for account in accounts:
+        serial = GenerateUUID()
+        expiry = datetime(2025, 6, 15, 8, 0, 0)
+        active = True
+        InsertParkbizz(serial, active, expiry, account.account_number)
     
 def GenerateParkbizzes():
     accounts = GetAccounts()
@@ -97,7 +130,20 @@ def GenerateParkingActivity():
     for bizz in bizzes:
         GenerateBizzActivity(bizz.serial_number)
 
+# List of ParkingActivity(type - ENTRANCE or EXIT, time: datetime)
 def GetParkingActivity(parkbizz_serial):
     return GetLatestActivity(parkbizz_serial)
+
+def FlipOccupiedStatus(type, number):
+    FlipStatus(type, number)
+
+def DataclassListToJson(list):
+    list_dict = []
+    for item in list:
+        list_dict.append(dataclasses.asdict(item))
+    return json.dumps(list_dict)
+
+def UpdateSpotStatus(spot_type, spot_number, occupied):
+    UpdateSpot(spot_type, spot_number, occupied)
 
 
